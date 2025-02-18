e4fda3d382d7cd00f4485edcb1c94ce4d39c2e9e
Newaz100 <yeasinnewaz100@gmail.com>
2025-02-19 01:51:53 +0600

    valid Contact

--- problems.js
+++ problems.js
@@ -14,6 +14,29 @@ console.log(calculateVAT(1000));
 console.log(calculateVAT(-50));
 
 
+function  validContact( contact ) {
+    if( typeof contact === "string" && contact.length == 11 && contact.startsWith('01') && !contact.includes(" ") ){
+
+        return true;
+    }
+    else if( typeof contact === "string" ){
+
+        return false;
+    }
+    else{
+        return "Invalid";
+    }
+}
+
+
+console.log(validContact("01712345678"));
+console.log(validContact("12345678901")); 
+
+
+
+
+
+
 
 
 
