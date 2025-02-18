fecf4361161795d20845210b582af5ae77125209
Newaz100 <yeasinnewaz100@gmail.com>
2025-02-19 01:44:33 +0600

    calculate VAT

--- problems.js
+++ problems.js
@@ -12,3 +12,108 @@ function calculateVAT( price ) {
 
 console.log(calculateVAT(1000));
 console.log(calculateVAT(-50));
+
+
+function  validContact( contact ) {
+    if( typeof contact === "string" && contact.length == 11 && contact.startsWith('01') && !contact.includes(" ") ){
+
+        return true;
+    }
+    else if( typeof contact === "string" ){
+
+        return false;
+    }
+    else{
+        return "Invalid";
+    }
+}
+
+
+
+
+
+
+function  willSuccess( marks ) {
+
+    if(!Array.isArray(marks)){
+        return "Invalid";
+    }
+
+    let passCount = 0, failCount = 0;
+    for(const mark of marks){
+        if (typeof mark !== "number" || mark < 0 || mark > 100) {
+            return "Invalid"; 
+        }
+
+        if (mark >= 50) {
+            passCount++;
+        } else {
+            failCount++;
+        }
+        
+    }
+    if(passCount>failCount){
+            return true;
+        }
+    else{
+        return false;
+    }
+}
+
+
+
+
+function  validProposal( person1 , person2 ){
+    if (
+        typeof person1 !== "object" || person1 === null ||
+        typeof person2 !== "object" || person2 === null ||
+        typeof person1.name !== "string" || typeof person2.name !== "string" ||
+        typeof person1.age !== "number" || person1.age < 0 ||
+        typeof person2.age !== "number" || person2.age < 0 ||
+        (person1.gender !== "male" && person1.gender !== "female") ||
+        (person2.gender !== "male" && person2.gender !== "female")
+    ){
+        
+            return "Invalid";
+    }
+    
+    if (person1.gender !== person2.gender && Math.abs(person1.age - person2.age) <= 7) {
+            return true;
+    }
+
+    return false;
+}
+
+
+
+
+
+function  calculateSleepTime( times ) {
+    let totalSecond = 0;
+    let hour = 0;
+    let min = 0;
+    let sec = 0;
+    if(!Array.isArray(times)){
+        return "Invalid";
+    }
+
+    for (let i = 0; i < times.length; i++) {
+        if (typeof times[i] !== 'number' || times[i] <= 0) {
+            return "Invalid";
+        }
+    }
+
+    for(const time of times){
+        totalSecond = totalSecond + time;
+        hour = Math.floor(totalSecond / 3600);
+        min = Math.floor((totalSecond % 3600) / 60);
+        sec = totalSecond % 60;
+    }
+    
+
+    return {hour: hour,minute: min,second: sec};
+}
+
+
+
+
